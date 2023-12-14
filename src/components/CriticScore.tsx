fc91324f817b1ff78a46967310cc166bd10d7bdd
Shahzaib Iqbal <ishahzaib07@gmail.com>
2023-12-14 01:47:18 -0800

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge
+      fontSize="14px"
+      paddingX={2}
+      borderRadius={"3px"}
+      colorScheme={color}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
