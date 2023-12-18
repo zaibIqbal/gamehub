d44635a9fcca30132f72d289c6081d8cf3517b6a
Shahzaib Iqbal <ishahzaib07@gmail.com>
2023-12-18 01:32:42 -0800

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,8 @@ const useGames = (gameQuery: GameQuery) =>
         queryFn: ({ pageParam = 1 }) => apiClient.getAll({ params: { genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText, page: pageParam } }),
         getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined;
-        }
+        },
+        staleTime: 24 * 60 * 60 * 1000
     });
 
 export default useGames;
