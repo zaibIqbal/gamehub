80b99656ced696adcafe0a123c8a0f706913af7c
Shahzaib Iqbal <ishahzaib07@gmail.com>
2023-12-21 07:22:39 -0800

    Build game screenshots

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -13,19 +13,20 @@ const axiosInstance = axios.create({
 })
 
 class APIClient<T>{
-    endpoinet: string;
+    endpoint: string;
 
     constructor(endpoint: string) {
-        this.endpoinet = endpoint
+        this.endpoint = endpoint
     }
 
     getAll = (config?: AxiosRequestConfig) => {
-        return axiosInstance.get<FetchResponse<T>>(this.endpoinet, config).then(res => res.data)
+        return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data)
     }
 
     get = (id: number | string) => {
-        return axiosInstance.get<T>(this.endpoinet + "/" + id).then(res => res.data)
+        return axiosInstance.get<T>(this.endpoint + "/" + id).then(res => res.data)
     }
+
 }
 
 export default APIClient;
